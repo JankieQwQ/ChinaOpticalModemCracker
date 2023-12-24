8024dcb66e5c65ffab555b22d59a5a02987e56ed
Jankie <notflyloong@outlook.com>
2023-12-24 17:21:24 +0800

    Add function for get default username and password

--- main.py
+++ main.py
@@ -13,3 +13,24 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+
+import sys
+import uuid
+import socket
+import urllib.request
+
+default = '''
+|       ISP        |   Username   |  Password   |
+China Telecom Anhui  telecomadmin    nE7jA%5m
+China Mobile         CMCCAdmin       aDm8H%MdA
+China United Network CUAdmin         CUAdmin
+'''
+
+class cracker:
+    def __init__(self,catIP='192.168.1.1'):
+        self.ip = catIP
+    
+    def default():
+        print(default)
+    
+    
