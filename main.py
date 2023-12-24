0cef373ba8d8c561165a78c32de96281665d17ee
Jankie <notflyloong@outlook.com>
2023-12-24 18:04:35 +0800

    Update Telnet

--- main.py
+++ main.py
@@ -35,16 +35,23 @@ China Optical Modem Cracker -
 
 0. Get default admin username and password
 1. Open TELNET (Only China Mobile)
-2. Crack
+2. Crack Optical Modem
+3. Telnet Client
 3. Exit
 '''
 
 class TelnetClient:
     def __init__(self):
         self.tn = telnetlib.Telnet()
+        self.prompt = b'$ '  # 设置命令提示符
 
     def login_host(self, host_ip, username, password):
-        self.tn.open(host_ip, port=23)
+        try:
+            self.tn.open(host_ip, port=23)
+        except Exception:
+            logging.warning(f'{host_ip}网络连接失败')
+            return False
+        
         self.tn.read_until(b'login: ', timeout=10)
         self.tn.write(username.encode('ascii') + b'\n')
         
@@ -55,15 +62,27 @@ class TelnetClient:
         command_result = self.tn.read_very_eager().decode('ascii')
         
         if 'Login incorrect' not in command_result:
+            logging.warning(f'{host_ip}登录成功')
             return True
         else:
-            logging.warning(f'Error username or password:{host_ip}')
+            logging.warning(f'{host_ip}登录失败，用户名或密码错误')
             return False
 
     def execute_some_command(self, command):
         self.tn.write(command.encode('ascii')+b'\n')
         time.sleep(2)
         command_result = self.tn.read_very_eager().decode('ascii')
+        logging.warning(f'命令执行结果：\n{command_result}')
+
+    def start_terminal(self):
+        while True:
+            command = input(self.prompt.decode('ascii'))
+            if command.lower() == 'exit':
+                break
+            self.tn.write(command.encode('ascii') + b'\n')
+            time.sleep(2)
+            command_result = self.tn.read_very_eager().decode('ascii')
+            print(command_result)
 
     def logout_host(self):
         self.tn.write(b"exit\n")
@@ -111,4 +130,15 @@ def main() -> int:
         crack.default()
     elif caseu == 1:
         webbrowser.open_new_tab('http://{}/telnet-config.asp'.format(ip))
+    elif caseu == 2:
+        pass
+    elif caseu == 3:
+        host_ip = '192.168.220.129'
+        username = 'root'
+        password = 'abcd1234'
+        command = 'whoami'
+        telnet_client = TelnetClient()
+        if telnet_client.login_host(host_ip, username, password):
+            telnet_client.execute_some_command(command)
+            telnet_client.logout_host()
     return 0
